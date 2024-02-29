413278060f52aa03bb605aa70ce05c313e8b7d9e
Тетяна Жульєва <128372881+TanyaJulieva@users.noreply.github.com>
2024-02-29 15:10:07 +0100

    pagination

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,24 +1,26 @@
+import axios from 'axios';
+
 const elem = {
   input: document.querySelector('.input-js'),
   loader: document.querySelector('.js-loader'),
 };
 
-async function getImages() {
+async function getImages(currentPage, query) {
   try {
-    const key = '42470360-53d39d66a700555aa13e5c54c';
-    const baseUrl = 'https://pixabay.com/api/';
-    const query = elem.input.value;
-    const link = `${baseUrl}?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+    const searchParams = new URLSearchParams({
+      page: currentPage,
+      per_page: 15,
+      key: '42470360-53d39d66a700555aa13e5c54c',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
 
-    const response = await fetch(link);
-
-    if (!response.ok) {
-      throw new Error('Image error!');
-    }
-
-    const data = await response.json();
-
-    return data;
+    const response = await axios.get(
+      `https://pixabay.com/api/?${searchParams}`
+    );
+    return response.data;
   } catch (error) {
     alert('Error while fetching images from pixabay!');
   } finally {
--- src/main.js
+++ src/main.js
@@ -7,34 +7,32 @@ import { getImages } from './js/pixabay-api.js';
 import { createMarkup } from './js/render-functions.js';
 
 const elem = {
-  input: document.querySelector('.input-js'),
   form: document.querySelector('.form-js'),
   list: document.querySelector('.gallery-js'),
   loader: document.querySelector('.js-loader'),
+  loadMoreBtn: document.querySelector('.js-load-more'),
+  loaderLoadMore: document.querySelector('.js-loader-load-more'),
 };
-
-elem.loader.classList.add('hidden');
 
 elem.form.addEventListener('submit', handlerSubmit);
 
 const gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-//   if (elem.list.childElementCount > 0) {
-//     elem.list.innerHTML = '';
-//   }
+let currentPage;
+let currentQuery;
 
 function handlerSubmit(evt) {
   elem.loader.classList.remove('hidden');
   evt.preventDefault();
 
-  if (elem.list.childElementCount > 0) {
-    elem.list.innerHTML = '';
-  }
+  const query = evt.target.elements.query.value;
 
-  getImages().then(data => {
+  elem.list.innerHTML = '';
+
+  getImages(currentPage, query).then(data => {
     const images = data.hits;
 
     if (images.length === 0) {
@@ -48,10 +46,41 @@ function handlerSubmit(evt) {
       return;
     }
 
-    elem.list.innerHTML = createMarkup(images);
+    elem.list.insertAdjacentHTML('beforeend', createMarkup(images));
 
+    elem.loadMoreBtn.classList.replace('load-more-hidden', 'load-more');
     gallery.refresh();
-  })
+
+    currentQuery = query;
+    currentPage = 1;
+  });
 
   evt.currentTarget.reset();
+  
+}
+
+elem.loadMoreBtn.addEventListener('click', handlerLoadMore);
+
+function handlerLoadMore() {
+  // elem.loaderLoadMore.classList.remove('hidden');
+  currentPage += 1;
+  
+
+  console.log(currentPage);
+
+  getImages(currentPage, currentQuery).then(data => {
+    const images = data.hits;
+
+    elem.list.insertAdjacentHTML('beforeend', createMarkup(images));
+
+    gallery.refresh();
+
+    console.log(data.totalHits);
+
+    // if(data.totalHits >= 500) {
+    //   elem.loadMoreBtn.classList.replace('load-more', 'hidden')
+    // }
+  })
+  elem.loader.remove()
+
 }
